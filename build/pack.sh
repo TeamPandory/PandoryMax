c75f60fc354a170880d1cde7ca67a26adf7284f3
dajoho <dajoho@teampandory.com>
2023-10-11 18:13:15 +0200

    more cool stuff

--- build/pack.sh
+++ build/pack.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 pushd ..
 git rev-parse --short HEAD > pandory.ver
+git checkout -- res/*.png
+VERCODE="PANDORYMAX-$(date +'%Y%m%d')-$(cat pandory.ver)"
+#echo $VERCODE;exit
+convert -pointsize 10 -fill white -draw "text 1104,710 '""${VERCODE^^}""'" res/default_bg.png res/default_bg.png
 tar --exclude="build" --exclude=".git" --exclude=".gitignore" -cvf build/pandory.tgz .
 popd
 
