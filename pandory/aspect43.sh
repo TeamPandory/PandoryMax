7765f717ce9fd0ec6b40e066a14b896a6939ce17
dajoho <dajoho@teampandory.com>
2023-10-18 13:46:55 +0200

    some more fixes

--- pandory/aspect43.sh
+++ pandory/aspect43.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sed -i 's#aspect_ratio_index = ".*"#aspect_ratio_index = "22"#g' /sdcard/retroarch/zip.cfg
-aplay /sdcard/pandory/fourthree.wav
+sed -i 's#aspect_ratio_index = ".*"#aspect_ratio_index = "22"#g' /sdcard/retroarch/zip_dc.cfg
+sed -i 's#aspect_ratio_index = ".*"#aspect_ratio_index = "22"#g' /sdcard/retroarch_lg/zip_guangqiang.cfg
 killall -9 retroarch
